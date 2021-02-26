b8a336ff22dc3ec21f937fb1fe7d40c5b11b0d31
MitchBunks <mitchbunkers@gmail.com>
2021-02-25 22:42:04 -0600

    edit

--- examples/pi_test.py
+++ examples/pi_test.py
@@ -30,7 +30,7 @@ try:
     image = Image.new('1', (epd.height, epd.width), 255)  # 255: clear the frame   
     draw = ImageDraw.Draw(image)
     
-    draw.text((120, 60), 'Hello World!', font = font15, fill = 0)
+    draw.text((60, 60), 'Hello World!', font = font15, fill = 0)
     epd.display(epd.getbuffer(image))
     time.sleep(2)
     
